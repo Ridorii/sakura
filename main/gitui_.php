9b885411a160d8faeacdd511b66a4f025333a48e
Flashwave <me@flash.moe>
2015-03-08 05:03:27 +0100

    just kidding this shit didn't actually work

--- main/gitui_.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_REQUEST['pull'])) {
-    shell_exec('git -C /var/www/flashii.net pull > /var/www/autopull.log');
-    print '<h2>Site should be up with the latest commit now.</h2>';
-}
-?>
-<form method="post" action="">
-    <input type="submit" name="pull" value="Pull from Repository" />
-</form>
