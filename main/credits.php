6a125d49fd3e2426b7054c7bec4fac71637326e9
Flashwave <me@flash.moe>
2015-04-01 17:56:51 +0200

    /r/php

---
+++ main/credits.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Sakura Main Index
+ */
+
+// Declare Namespace
+namespace Sakura;
+
+// Include components
+require_once '/var/www/flashii.net/_sakura/sakura.php';
+
+// Add page specific things
+$renderData['page'] = [
+    'title' => 'Flashii Dev'
+];
+$renderData['newsPosts'] = Main::getNewsPosts(3);
+
+// Print page contents
+print Main::$_TPL->render('main/index.tpl', $renderData);
