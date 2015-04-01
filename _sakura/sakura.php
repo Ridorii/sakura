73992c14e0b9b093ed95973f2d8c0b3ce8d973a6
Flashwave <me@flash.moe>
2015-04-01 17:35:27 +0200

    error logging

--- _sakura/sakura.php
+++ _sakura/sakura.php
@@ -7,33 +7,35 @@
 // Declare namespace
 namespace Sakura;
 
+// Define Sakura version
+define('SAKURA_VERSION', '20150330');
+
+// Define Sakura Path
+define('ROOT', str_replace('_sakura', '', dirname(__FILE__)));
+
 // Error Reporting: 0 for production and -1 for testing
 error_reporting(-1);
+ini_set('log_errors', 1);
+ini_set('error_log', ROOT .'errors.log');
 
 // Start output buffering with gzip and no gzip fallback
 if(!ob_start('ob_gzhandler'))
     ob_start();
 
-// Define Sakura version
-define('SAKURA_VERSION', '20150330');
-
-// Define Sakura Path
-define('ROOT_DIRECTORY', str_replace('_sakura', '', dirname(__FILE__)));
-
 // Include Configuration
-require_once ROOT_DIRECTORY .'_sakura/config/config.php';
+require_once ROOT .'_sakura/config/config.php';
 
 // Include libraries
-require_once ROOT_DIRECTORY .'_sakura/vendor/autoload.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Main.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Hashing.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Configuration.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Templates.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Sessions.php';
-require_once ROOT_DIRECTORY .'_sakura/components/Users.php';
+require_once ROOT .'_sakura/vendor/autoload.php';
+require_once ROOT .'_sakura/components/Main.php';
+require_once ROOT .'_sakura/components/Hashing.php';
+require_once ROOT .'_sakura/components/Configuration.php';
+require_once ROOT .'_sakura/components/Templates.php';
+require_once ROOT .'_sakura/components/Sessions.php';
+require_once ROOT .'_sakura/components/Users.php';
 
 // Generate path to database driver
-$_DBNGNPATH = ROOT_DIRECTORY .'_sakura/components/database/'. $sakuraConf['db']['driver'] .'.php';
+$_DBNGNPATH = ROOT .'_sakura/components/database/'. $sakuraConf['db']['driver'] .'.php';
 
 // Include database driver
 if(file_exists($_DBNGNPATH))
