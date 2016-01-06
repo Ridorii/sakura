a4621bd05161465397e09243566eb3083c8d9b44
Julian van de Groep <me@flash.moe>
2016-01-06 23:33:22 +0100

    r20160106

--- sakura.php
+++ sakura.php
@@ -8,7 +8,7 @@
 namespace Sakura;
 
 // Define Sakura version
-define('SAKURA_VERSION', '20160104');
+define('SAKURA_VERSION', '20160106');
 define('SAKURA_VLABEL', 'Eminence');
 define('SAKURA_COLOUR', '#6C3082');
 
