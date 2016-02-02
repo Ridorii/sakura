2882cb7900b3d72fa507986cf6e24a9e4db820a9
Flashwave <me@flash.moe>
2016-02-02 13:48:33 +0100

    Serve scripts

---
+++ serve.bat
@@ -0,0 +1,3 @@
+@echo off
+echo This script assumes php.exe is in the PATH variable
+php -S localhost:8000 -t public/ server.php
---
+++ serve.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'This script assumes php is in the PATH variable'
+php -S localhost:8000 -t public/ server.php
