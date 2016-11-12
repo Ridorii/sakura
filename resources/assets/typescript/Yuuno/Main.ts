5ce404b6a603f96277c3b91f882e346c5735db26
Julian van de Groep <me@flash.moe>
2016-11-12 02:52:51 +0100

    topic view and comments remaining

--- resources/assets/typescript/Yuuno/Main.ts
+++ resources/assets/typescript/Yuuno/Main.ts
@@ -10,10 +10,6 @@ namespace Yuuno
             Sakura.Friend.Init();
             Notifications.Init();
             Busy.Init();
-
-            if (window.location.pathname === '/' || window.location.pathname === '/forum' || window.location.pathname === '/forum/') {
-                Ybabstat.Initiate();
-            }
         }
     }
 }
